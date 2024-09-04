da18eeabb7ac589d0c1f75b5ad1e68ea2ba46b45
R4bbitzzzz <agostonpalatinus@gmail.com>
2024-09-04 09:02:11 +0200

    Update app.js

--- Színezés_órai_ism/js/app.js
+++ Színezés_órai_ism/js/app.js
@@ -1,12 +1,34 @@
 // alert("asdfa")
 //Számokat tartalmazó négyzetek létrehozása.
-const makeBoxes = () => {
-    let boxList = [];
-    for (let i = 1; i < 16; i++) {
-        boxList.push(`<div id="box-${i}" class="box">${i}</div>`);
-    }
-    return boxList;
-};
+// const makeBoxes = () => {
+//     let boxList = [];
+//     for (let i = 1; i < 16; i++) {
+//         boxList.push(`<div id="box-${i}" class="box">${i}</div>`);
+//     }
+//     return boxList;
+// };
+
+const makeBoxes = () =>{
+    const data = [
+        {number: 1},
+        {number: 2},
+        {number: 3},
+        {number: 4},
+        {number: 5},
+        {number: 6},
+        {number: 7},
+        {number: 8},
+        {number: 9},
+        {number: 10},
+        {number: 11},
+        {number: 12},
+        {number: 13},
+        {number: 14},
+        {number: 15},
+        
+    ]
+}
+
 //Négyzetek megjelenítése HTML-ben
 const renderBoxes = () => {
     const boxesContainer = document.querySelector(".boxes");
@@ -28,3 +50,4 @@ document.addEventListener("DOMContentLoaded", () =>{
     const boxes = document.querySelectorAll(".box") 
     console.log(`boxLists3`,boxes); //log
 });
+
