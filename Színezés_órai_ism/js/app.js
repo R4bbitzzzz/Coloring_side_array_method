71d8d67958f14788578a8cef7832a79d0459a60c
R4bbitzzzz <agostonpalatinus@gmail.com>
2024-09-04 08:33:48 +0200

    Update app.js

--- Színezés_órai_ism/js/app.js
+++ Színezés_órai_ism/js/app.js
@@ -7,14 +7,24 @@ const makeBoxes = () => {
     }
     return boxList;
 };
-
+//Négyzetek megjelenítése HTML-ben
 const renderBoxes = () => {
     const boxesContainer = document.querySelector(".boxes");
     boxesContainer.innerHTML = "";
     boxesContainer.innerHTML = makeBoxes().join('');
+
+    //const boxes = document.querySelectorAll(".box") 
+    //console.log(`boxLists2`,boxes); //log
 };
 
 //Oldal betöltésekor inicializálodik a weboldal.
-document.addEventListener("DOMContentLoaded", renderBoxes);
-const boxes = document.querySelectorAll(".box")
-console.log(boxes);
+//document.addEventListener("DOMContentLoaded", renderBoxes);
+//ezek a sorok hamarabb futnak le mint a callback függvények, ezért itt még nem létezik a renderelt tartalom.
+//const boxes = document.querySelectorAll(".box") 
+//console.log(`boxLists1 `,boxes); //log
+
+document.addEventListener("DOMContentLoaded", () =>{
+    renderBoxes();
+    const boxes = document.querySelectorAll(".box") 
+    console.log(`boxLists3`,boxes); //log
+});
