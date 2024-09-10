e182c39bc5723fac8aba7557a17bb61acc44908d
R4bbitzzzz <agostonpalatinus@gmail.com>
2024-09-10 12:32:36 +0200

    Update app.js

--- Színezés_órai_ism/js/app.js
+++ Színezés_órai_ism/js/app.js
@@ -1,12 +1,13 @@
 // alert("asdfa")
-//Számokat tartalmazó négyzetek létrehozása.
-// const makeBoxes = () => {
-//     let boxList = [];
-//     for (let i = 1; i < 16; i++) {
-//         boxList.push(`<div id="box-${i}" class="box">${i}</div>`);
-//     }
-//     return boxList;
-// };
+
+//Számokat tartalmazó négyzetek létrehozás
+/* const makeBoxes = () => {
+    let boxList = [];
+    for (let i = 1; i < 16; i++) {
+        boxList.push(`<div id="box-${i}" class="box">${i}</div>`);
+    }
+    return boxList;
+}; */
 
 const makeBoxes = () =>{
     const data = [
@@ -24,30 +25,65 @@ const makeBoxes = () =>{
         {number: 12},
         {number: 13},
         {number: 14},
-        {number: 15},   
+        {number: 15}
     ];
-    const content = data.map(({number}) => `<div id="box-${number}" class="box">${number}</div>`);
+    const content = data.map( ({number}) => `<div id="${number}" class="box">${number}</div>` );
+    return content
 }
 
-//Négyzetek megjelenítése HTML-ben
+const getInputValue = () =>{
+    return document.querySelector("#num").value;
+}
+
+const checkValue = () =>{
+    const value = getInputValue();
+    if(!value.trim()){
+        return [false,0];
+    }
+    if(isNaN(value)){
+        return [false,0];
+    }
+    const currentValue = Number(value)
+    if(currentValue < 1 ||currentValue > 15 ){
+        return [false,0];
+    }
+    return[true,currentValue]
+}
+
+const rndNum = () =>{
+    return Math.floor(Math.random()*256)
+}
+
+const randomColor = () =>{
+    const r = rndNum();
+    const g = rndNum();
+    const b = rndNum();
+    return [r,g,b]
+}
+
+const coloring = () =>{
+    const button = document.querySelector("#szinez")
+    button.addEventListener("click",randomColor);
+    const
+}
+
+//A négyzetek megjelenítése a HTML-ben
 const renderBoxes = () => {
     const boxesContainer = document.querySelector(".boxes");
     boxesContainer.innerHTML = "";
     boxesContainer.innerHTML = makeBoxes().join('');
-
-    //const boxes = document.querySelectorAll(".box") 
-    //console.log(`boxLists2`,boxes); //log
+    //const boxes = document.querySelectorAll(".box")
+    //console.log("boxlista1: ",boxes);
 };
-
-//Oldal betöltésekor inicializálodik a weboldal.
+//Az oldal betöltésekor inicializálódik a weboldal
 //document.addEventListener("DOMContentLoaded", renderBoxes);
-//ezek a sorok hamarabb futnak le mint a callback függvények, ezért itt még nem létezik a renderelt tartalom.
-//const boxes = document.querySelectorAll(".box") 
-//console.log(`boxLists1 `,boxes); //log
+
+//ezek a kódsorok hamarabb funak le mint a callback részek ezért itt még nem létezik a renderelt tartalom
+//const boxes = document.querySelectorAll(".box")
+//console.log("boxlista1: ",boxes);
 
 document.addEventListener("DOMContentLoaded", () =>{
     renderBoxes();
-    //const boxes = document.querySelectorAll(".box") 
-    //console.log(`boxLists3`,boxes); //log
+    const boxes = document.querySelector(".box");
+    
 });
-
